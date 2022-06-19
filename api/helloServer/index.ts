d03089b7f656c702322c99a33ae14bc51794853d
mathieujullien77190 <mathieu.jullien77190@gmail.com>
2022-06-20 01:42:33 +0200

    test type azure function

--- api/helloServer/index.ts
+++ api/helloServer/index.ts
@@ -1,6 +1,11 @@
 /** @format */
 
-const httpTrigger = async function (context, req): Promise<void> {
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+
+const httpTrigger: AzureFunction = async function (
+	context: Context,
+	req: HttpRequest
+): Promise<void> {
 	context.res.status(200).json({ data: "hello server" })
 }
 
